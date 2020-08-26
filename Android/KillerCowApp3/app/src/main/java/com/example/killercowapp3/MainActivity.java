6517dec840dc22b3113bebc7b31bedbcde89dba7
Chris Jenks <38247910+ChrisJenks96@users.noreply.github.com>
2020-08-26 16:00:40 +0100

    android irrlicht init

---
+++ Android/KillerCowApp3/app/src/main/java/com/example/killercowapp3/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.killercowapp3;
+
+import android.app.NativeActivity;
+import android.os.Bundle;
+
+public class MainActivity extends NativeActivity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
