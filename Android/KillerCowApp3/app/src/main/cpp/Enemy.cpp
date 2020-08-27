5bd7a4446e790295cd4b44a4c48ae9c26ad8a25b
Chris Jenks <38247910+ChrisJenks96@users.noreply.github.com>
2020-08-27 02:34:45 +0100

    android is a mess, md2 with no lighting works, md2 with lighting has no textures, obj loads no textures at all, lighting or not.

--- Android/KillerCowApp3/app/src/main/cpp/Enemy.cpp
+++ Android/KillerCowApp3/app/src/main/cpp/Enemy.cpp
@@ -1,6 +1,6 @@
 #include "Enemy.h"
 
-Enemy::Enemy(IrrlichtDevice* d)
+Enemy::Enemy(IrrlichtDevice* d, const float distAway)
 {
 	IVideoDriver* driver = d->getVideoDriver();
 	ISceneManager* smgr = d->getSceneManager();
@@ -8,34 +8,117 @@ Enemy::Enemy(IrrlichtDevice* d)
 	if (mesh)
 	{
 		node = smgr->addAnimatedMeshSceneNode(mesh);
-		node->setPosition(vector3df(0.0f, 0.0f, 15.0f));
+
+		RandomPosition(distAway);
 		node->setScale(vector3df(0.05f, 0.05f, 0.05f));
 		if (node)
 		{
-			node->setMaterialFlag(EMF_LIGHTING, false);
+			node->setMaterialFlag(EMF_LIGHTING, true);
 			node->setMD2Animation(scene::EMAT_RUN);
 			node->setMaterialTexture(0, driver->getTexture("media/sydney.bmp"));
 		}
 	}
 }
 
-void Enemy::LookAt(const vector3df p, const float offset)
+void Enemy::RandomPosition(const float distAway)
 {
-	core::vector3df Rot;
-	float X; float Y;
-	X = p.X - node->getPosition().X;
-	Y = p.Z - node->getPosition().Z;
-	Rot.Y = (irr::f32)(((atan2(X, Y) * 180 / PI2) + 180) + offset);
-	node->setRotation(Rot);
+	float x = rand() % 721 + (-360);
+	float z = rand() % 721 + (-360);
+	vector3df target = vector3df(x, 0.0f, z);
+	vector3df p = (target - node->getPosition()).normalize() * distAway;
+	node->setPosition(p);
 }
 
-void Enemy::MoveTowards(const vector3df p, const float speed) 
+void Enemy::LookAt(const vector3df p, const float offset)
 {
-	vector3df dir = (p - node->getPosition()).normalize();
-	node->setPosition(node->getPosition() + (dir * speed));
+	const vector3df toTarget = p - node->getPosition();
+	node->setRotation(toTarget.getHorizontalAngle() + vector3df(0.0f, offset, 0.0f));
+}
+
+void Enemy::Attack(const float dt) 
+{
+	currAttackLength += 1.0f * dt;
+	//do some attacky stuff here...
+	if (currAttackLength > attackLength) {
+		isAttacking = false;
+		currAttackLength = 0.0f;
+		node->setMD2Animation(scene::EMAT_RUN);
+	}
+}
+
+ENEMY_STATE Enemy::MoveTowards(const vector3df p, const float speed) 
+{
+	float distance = (p - node->getPosition()).getLengthSQ();
+	if (isAttacking && attackOnce)
+		return ENEMY_STATE::ATTACK;
+
+	else if ((distance < attackDistance)) {
+		if (!attackOnce) {
+			isAttacking = true;
+			attackOnce = true;
+			node->setMD2Animation(scene::EMAT_ATTACK);
+			return ENEMY_STATE::ATTACK;
+		}
+	}
+
+	if (!isAttacking) {
+		vector3df dir = (p - node->getPosition()).normalize();
+		node->setPosition(node->getPosition() + (dir * speed));
+	}
+
+	if (distance < 0.5f) {
+		node->setMD2Animation(scene::EMAT_DEATH_FALLBACK);
+		return ENEMY_STATE::RESET;
+	}
+
+	return ENEMY_STATE::NONE;
+}
+
+void Enemy::Reset()
+{
+	float distAway = rand() % (50 + 1) + 20;
+	node->setMD2Animation(scene::EMAT_RUN);
+	RandomPosition(distAway);
+	attackOnce = false;
 }
 
 Enemy::~Enemy()
 {
 
+}
+
+EnemyFactory::EnemyFactory(IrrlichtDevice* d, const int size)
+{
+	//20 - 70
+	for (int i = 0; i < size; i++) {
+		float distAway = rand() % (50 + 1) + 20;
+		Enemy newEnemy(d, distAway);
+		//2.0 units away and for 2 seconds
+		newEnemy.SetAttackAttrib(10.0f, 2.0f);
+		enemies.push_back(newEnemy);
+	}
+}
+
+void EnemyFactory::Update(const vector3df t, const float dt)
+{
+	for (auto& x : enemies) {
+		x.LookAt(t, -90.0f);
+		ENEMY_STATE es = x.MoveTowards(t, 5.0f * dt);
+		switch (es)
+		{
+			case RESET:
+				x.Reset();
+				break;
+			case ATTACK:
+				x.Attack(dt);
+				break;
+			default:
+				break;
+		}
+	}
+}
+
+EnemyFactory::~EnemyFactory()
+{
+	enemies.clear();
 }
--- Android/KillerCowApp3/app/src/main/cpp/Player.cpp
+++ Android/KillerCowApp3/app/src/main/cpp/Player.cpp
@@ -5,14 +5,15 @@ Player::Player(IrrlichtDevice* d)
 	IVideoDriver* driver = d->getVideoDriver();
 	ISceneManager* smgr = d->getSceneManager();
 	mesh = smgr->getMesh("media/sydney.md2");
+
 	if (mesh)
 	{
 		node = smgr->addAnimatedMeshSceneNode(mesh);
-		node->setPosition(vector3df(0.0f, 0.0f, 0.0f));
+		node->setPosition(vector3df(3.0f, 0.0f, -3.0f));
 		node->setScale(vector3df(0.05f, 0.05f, 0.05f));
 		if (node)
 		{
-			node->setMaterialFlag(EMF_LIGHTING, false);
+			node->setMaterialFlag(EMF_LIGHTING, true);
 			node->setMD2Animation(scene::EMAT_STAND);
 			node->setMaterialTexture(0, driver->getTexture("media/sydney.bmp"));
 		}
@@ -21,12 +22,8 @@ Player::Player(IrrlichtDevice* d)
 
 void Player::LookAt(const vector3df p, const float offset)
 {
-	core::vector3df Rot;
-	float X; float Y;
-	X = p.X - node->getPosition().X;
-	Y = p.Z - node->getPosition().Z;
-	Rot.Y = (irr::f32)(((atan2(X, Y) * 180 / PI2) + 180) + offset);
-	node->setRotation(Rot);
+	const vector3df toTarget = p - node->getPosition();
+	node->setRotation(toTarget.getHorizontalAngle() + vector3df(0.0f, offset, 0.0f));
 }
 
 Player::~Player()
